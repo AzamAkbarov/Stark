862aa4c965572da00187e112c42fb5711e11693f
AzamAkbarov <99525878+AzamAkbarov@users.noreply.github.com>
2022-06-07 14:30:44 +0500

    initial commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-       /*===== Resize Navbar on Scroll =====*/
+       
        var navbar = document.querySelector(".navbar");
-       // when the scroll is higher than 20 viewport height, add the sticky classs to the tag with a class navbar 
+  
        window.onscroll = () =>{
        this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
      }
@@ -14,7 +14,7 @@
               navMenu.classList.toggle("active");
           })
       }
-      // closing menu when link is clicked
+      // закрытие меню при переходе по ссылке
       const navLink = document.querySelectorAll(".nav-link");
       function linkAction()
       {
@@ -43,20 +43,20 @@
           })
       }
       window.addEventListener('scroll', scrollActive)
-      /*===== Skills Animation =====*/
+      /*===== Bar =====*/
       const skills_wrap = document.querySelector(".about-skills"),
       skills_bar = document.querySelectorAll(".progress-line");
       window.addEventListener("scroll", () => {
           skillsEffect();
       })
-      // every time we scroll checking, we exceeded the about-skills or not
+      // каждый раз прокручиваем проверяем, превысили мы about-skills или нет
       function checkScroll(el)
       {
-          //getting the top position of about-skills relative to view port, in other words we need to get
-          // amount of pixels between about-skills and the top edge of the window.
+          //получение верхней позиции about-skills относительно порта просмотра, другими словами нам нужно получить
+          // количество пикселей между about-skills и верхним краем окна.
           let rect = el.getBoundingClientRect();
-          // after knowing the amount of pixels between the top edge of about skills and the top edge of window
-          // now we will check we exceeded the bottom edge of about skills or not
+          // Зная количество пикселей между верхним краем информации о навыках и верхним краем окна
+          
           if(window.innerHeight >= rect.top + el.offsetHeight) return true;
           return false;
       }
